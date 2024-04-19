cfaa374d80634f7f9d10d5f3229491e86855fb33
RamziHkiri <6257@holbertonstudents.com>
2024-04-19 14:51:52 +0200

    task2

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,60 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+export const Director = class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+export const Teacher = class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+function isNumber(value: any): value is number {
+  return typeof value === 'number';
+}
+
+export function createEmployee(salary: number | string): DirectorInterface | TeacherInterface {
+  if (isNumber(salary) && salary < 500) {
+    return new Teacher();
+  }
+
+  return new Director();
+}
+
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is DirectorInterface {
+  return (employee as DirectorInterface).workDirectorTasks !== undefined;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if(isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
