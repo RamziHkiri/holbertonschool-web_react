906c0a15d78f4597a32f4187bc411f88db451c44
RamziHkiri <6257@holbertonstudents.com>
2024-05-12 13:52:06 +0200

    task 0

---
+++ React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import CourseListRow from './CourseListRow';
+import './CourseList.css';
+import PropTypes from 'prop-types';
+import CourseShape from './CourseShape';
+
+function CourseList({ listCourses = [] }) {
+    return (
+        <table>
+            <thead>
+                <CourseListRow isHeader={true} textFirstCell="Available courses" />
+                <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
+            </thead>
+            <tbody>
+            {listCourses.length > 0 ? (
+                    listCourses.map(course => (
+                        <CourseListRow
+                            key={course.id}
+                            isHeader={false}
+                            textFirstCell={course.name}
+                            textSecondCell={course.credit.toString()}
+                        />
+                    ))
+                ) : (
+                    <CourseListRow isHeader={false} textFirstCell="No course available yet" />
+                )}
+            </tbody>
+        </table>
+    )
+}
+CourseList.propTypes = {
+    listCourses: PropTypes.arrayOf(CourseShape).isRequired, // Define propTypes
+};
+export default CourseList;
