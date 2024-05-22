b01d281dad0fb1ac94212c266a3a1df43796a3e8
RamziHkiri <6257@holbertonstudents.com>
2024-05-22 14:29:31 +0200

    Correction

--- React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import BodySection from './BodySection';
-import './BodySection.css';
+import React from 'react';
 import PropTypes from 'prop-types';
+import BodySection from './BodySection';
+import { css } from 'aphrodite';
 
-class BodySectionWithMarginBottom extends Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div className="BodySectionWithMargin">
-				<BodySection {...this.props} />
-			</div>
-		);
-	};
+const BodySectionWithMarginBottom = ({ title, children }) => {
+  return (
+    <div className={css(styles.BodySection)}>
+      <BodySection title={title}>{children}</BodySection>
+    </div>
+  );
 };
 
 BodySectionWithMarginBottom.propTypes = {
-	title: PropTypes.string,
-	children: PropTypes.node
-}
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: '100px'
+  }
+}); 
+
 export default BodySectionWithMarginBottom;
